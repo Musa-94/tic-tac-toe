0bc64cfbc5b925b42c68ef2591dd8bf8cfbb3a8d
Musa-94 <x.m.m.o@mail.ru>
2021-03-25 19:18:27 +0400

    delete empty rows

--- src/modules/ticTacToe/actions.ts
+++ src/modules/ticTacToe/actions.ts
@@ -1,5 +1,4 @@
 import constants from '../../constants';
 import { TPayloadSetSymbolOnGameBoard } from '../../types';
 
-
 export const setPlayerSymbol = (payload: TPayloadSetSymbolOnGameBoard) => ({ type: constants.SET_NEW_SYMBOL_ON_GAME_BOARD, payload });
--- src/types/ticTacToeTypes.ts
+++ src/types/ticTacToeTypes.ts
@@ -10,7 +10,6 @@ export interface TicTacToeState {
     playerSymbol: 'o' | 'x';
 }
 
-
 export type TPayloadSetSymbolOnGameBoard = {
     rowPosition: string;
     elementIndex: number;

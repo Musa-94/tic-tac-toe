84d05ba6444168ae6a79c76671e5b9d8453c31c7
zabrat <zaur1996@bk.ru>
2021-03-27 11:04:15 +0300

    added button congigs

--- src/components/customButton/CustomButton.tsx
+++ src/components/customButton/CustomButton.tsx
@@ -2,29 +2,37 @@ import React from 'react';
 import { Button } from './styledComponents';
 
 type TProps = {
+    type?: 'submit' | 'reset' | 'button',
     fontSize?: number,
     textColor?: string,
     textTitle?: string;
     borderColor?: string,
     borderWidth?: number,
     borderStyle?: string,
+    handleClick?: React.MouseEventHandler<HTMLButtonElement>,
+    handleSubmit?: React.FormEventHandler<HTMLButtonElement>,
     backgroundColor?: string,
 }
 
 const CustomButton: React.FC<TProps> = props => {
     const {
+        type = 'button',
         fontSize = 20,
         textTitle = 'Button',
         textColor,
         borderColor,
         borderWidth,
         borderStyle,
+        handleClick,
+        handleSubmit,
         backgroundColor,
     } = props;
 
     return (
-        <Button type={'submit'}
+        <Button type={type}
                 children={textTitle}
+                onClick={handleClick}
+                onSubmit={handleSubmit}
                 fontSize={fontSize}
                 textColor={textColor}
                 borderColor={borderColor}
